05f14d13c69de0a8efda9d4521982e07ed3508d2
Kirill V <scriptythememer@gmail.com>
2025-01-09 06:48:13 +0700

    starting out

--- RbxlRepacker/Program.cs
+++ RbxlRepacker/Program.cs
@@ -1,3 +1,18 @@
-﻿Console.WriteLine(args.FirstOrDefault());
+﻿string? filePath = args.FirstOrDefault();
+
+if (filePath == null) {
+    Console.WriteLine("Drag and drop desired .rbxl file into exe.");
+    Console.ReadLine();
+    return;
+}
+
+var info = new FileInfo(filePath);
+if (info.Extension != ".rbxl") {
+    return;
+}
+
+using (FileStream stream = File.OpenRead(filePath)) {
+    BinaryReader reader = new(stream);
+}
 
 Console.ReadLine();
